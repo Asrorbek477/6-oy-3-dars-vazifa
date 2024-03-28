845c956ff3b0326b38d3ea3ce444fb69c3c5d9c6
Asrorbek477 <asrorbeksaidmurodov2004@gmail.com>
2024-03-29 00:30:47 +0500

    new proyekt

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+
+function ToDoList() {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  const handleAddTask = () => {
+    if (newTask) {
+      setTasks([...tasks, newTask]);
+      setNewTask("");
+    }
+  };
+
+  const handleRemoveTask = (index) => {
+    const updatedTasks = tasks.filter((_, i) => i !== index);
+    setTasks(updatedTasks);
+  };
+
+  return (
+    <div className="container">
+      <h1 className="title">Create your ToDo list</h1>
+      <input
+        type="text"
+        value={newTask}
+        onChange={(e) => setNewTask(e.target.value)}
+        placeholder="Enter a new task"
+        className="input__task"
+      />
+      <button onClick={handleAddTask} className="btn__task">
+        Add Task
+      </button>
+      <ul className="list__task">
+        {tasks.map((task, index) => (
+          <li key={index} className="item__task">
+            {task}
+            <button
+              onClick={() => handleRemoveTask(index)}
+              className="btn__remove"
+            >
+              Remove
+            </button>
+          </li>
+        ))}
+      </ul>
+      <div className="section__main">
+        {/* <li className="item">Get IELTS</li> */}
+      </div>
+      <div className="footer">
+        <p className="text__footer">Created by </p>
+        <p className="text__footer">Saidmurodov Asrorbek</p>
+      </div>
+    </div>
+  );
+}
+
+export default ToDoList;
